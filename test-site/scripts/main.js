4eca691bb76e5eef35dc8708569baabd9550e5f8
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-05-12 04:10:35 +1000

    add button to change user and display user name in heading

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,9 +1,31 @@
-var myImage = document.querySelector('img')
-myImage.onclick = function(){
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    if (myName !== null){
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myImage.onclick = function () {
     var imageSource = myImage.getAttribute('src');
-    if(imageSource === 'images/firefox-logo.png'){
+    if (imageSource === 'images/firefox-logo.png') {
         myImage.setAttribute('src', 'images/firefox-1-logo.png')
-    }else{
+    } else {
         myImage.setAttribute('src', 'images/firefox-logo.png')
     }
+}
+
+myButton.onclick = function () {
+    setUserName();
 }
