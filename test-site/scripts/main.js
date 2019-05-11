ba967beab90ba30da2179f115429b293d7105859
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-05-12 03:42:16 +1000

    js to change image on click

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,9 @@
-var heading = document.querySelector('h1');
-heading.textContent = 'Hello World!'
+var myImage = document.querySelector('img')
+myImage.onclick = function(){
+    var imageSource = myImage.getAttribute('src');
+    if(imageSource === 'images/firefox-logo.png'){
+        myImage.setAttribute('src', 'images/firefox-1-logo.png')
+    }else{
+        myImage.setAttribute('src', 'images/firefox-logo.png')
+    }
+}
